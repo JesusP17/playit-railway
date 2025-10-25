4a12ed522b51e8cab7a3a4c5604ff710da371519
JesusP17 <jesusperalta251@gmail.com>
2025-10-25 16:31:22 -0500

    Delete start.sh

--- start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Script para iniciar Playit.gg en Railway
-
-echo "Iniciando Playit.gg..."
-chmod +x Playit
-./Playit secret 69599ed0c7572b86f93bfe852722077d05163e1eb654941047a212babbed3742
